11b315688450e9ca6db64dd205b0a35f69720f8b
Manuel Estrada <manuel.estrada16@gmail.com>
2022-10-08 10:59:49 -0400

    added validations

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,27 +5,40 @@ function show(data) {
     return (
         <Def>
             <main>
-                <h1>{data.place.name}</h1>
-                <img src={`${data.place.pic}`} />
-                <h2 className='main'>{data.place.cuisines}</h2>
-                <h4 className='main'>{data.place.city},{data.place.state}</h4>
-                <h5 className='main'>
-                    Rating
-                    <input className="form-control" id="rating" />
-                </h5>
-                <h5 className='main'>
-                    Comments
-                    <input className="form-control" id="comments" />
-                </h5>
-                <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-                    Edit
-                </a>
-                <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-                    <button type="submit" className="btn btn-danger">
-                        Delete
-                    </button>
-                </form>
-
+                <div className="row">
+                    <div className="col-sm-6">
+                        <img src={`${data.place.pic}`} />
+                        <h3>
+                            Located in {data.place.city}, {data.place.state}
+                        </h3>
+                    </div>
+                    <div className="col-sm-6">
+                        <h1>{data.place.name}</h1>
+                        <h2>
+                            Rating
+                        </h2>
+                        <br />
+                        <h2>
+                            Description
+                        </h2>
+                        <h3>
+                            {data.place.showEstablished()}
+                        </h3>
+                        <h4>
+                            Serving {data.place.cuisines}
+                        </h4>
+                        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+                            Edit
+                        </a>
+                        <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+                            <button type="submit" className="btn btn-danger">
+                                Delete
+                            </button>
+                        </form>
+                    </div>
+                </div>
+                <hr />
+                <h2>Comments</h2>
             </main>
         </Def>
     )
