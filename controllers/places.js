e7bbc5c40ebd0514cf7c22a13f7c7c95d4e95ff2
Manuel Estrada <manuel.estrada16@gmail.com>
2022-10-10 15:33:08 -0400

    display comment on places page

--- controllers/places.js
+++ controllers/places.js
@@ -109,7 +109,7 @@ const router = require('express').Router()
 const db = require('../models')
 
 router.get('/', (req, res) => {
-    db.Place.find()
+  db.Place.find()
     .then((places) => {
       res.render('places/index', { places })
     })
@@ -121,23 +121,23 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
   db.Place.create(req.body)
-  .then(() => {
+    .then(() => {
       res.redirect('/places')
-  })
-  .catch(err => {
+    })
+    .catch(err => {
       if (err && err.name == 'ValidationError') {
-          let message = 'Validation Error: '
-          for(var field in err.errors) {
-            message += `${field} was ${err.errors[field].value}.`
-            message += `${err.errors[field].message}` 
-          }
-          console.log('Validation error message', message)
-          res.render('places/new', {message})
+        let message = 'Validation Error: '
+        for (var field in err.errors) {
+          message += `${field} was ${err.errors[field].value}.`
+          message += `${err.errors[field].message}`
+        }
+        console.log('Validation error message', message)
+        res.render('places/new', { message })
       }
       else {
-          res.render('error404')
+        res.render('error404')
       }
-  })
+    })
 })
 
 
@@ -147,14 +147,17 @@ router.get('/new', (req, res) => {
 
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
-  .then(place => {
+    .populate('comments')
+    .then(place => {
+      console.log(place.comments)
       res.render('places/show', { place })
-  })
-  .catch(err => {
+    })
+    .catch(err => {
       console.log('err', err)
       res.render('error404')
-  })
+    })
 })
+
 
 router.put('/:id', (req, res) => {
   res.send('PUT /places/:id stub')
@@ -173,7 +176,7 @@ router.post('/:id/rant', (req, res) => {
 })
 
 router.delete('/:id/rant/:rantId', (req, res) => {
-    res.send('GET /places/:id/rant/:rantId stub')
+  res.send('GET /places/:id/rant/:rantId stub')
 })
 
 module.exports = router
