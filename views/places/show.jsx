cd33f1cfc92b7d583b150d5e43483b82cbc6272b
Manuel Estrada <manuel.estrada16@gmail.com>
2022-10-15 02:06:43 -0400

    added bonus to delete comments

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -18,7 +18,7 @@ function show(data) {
         }, 0)
         let averageRating = Math.round(sumRatings / data.place.comments.length)
         let stars = ''
-        for (let i = 0; i <averageRating; i++) {
+        for (let i = 0; i < averageRating; i++) {
             stars += '☆'
         }
         rating = (
@@ -26,18 +26,24 @@ function show(data) {
                 {stars} stars
             </h3>
         )
-        comments = data.place.comments.map(c => {
-            return (
-                <div className="border">
-                    <h2 className="rant">{c.rant ? 'Rant!' : 'Rave!'}</h2>
-                    <h4>{c.content}</h4>
-                    <h3>
-                        <stong>- {c.author}</stong>
-                    </h3>
-                    <h4>Rating: {c.stars}</h4>
-                </div>
-            )
-        })
+
+        if (data.place.comments.length) {
+            comments = data.place.comments.map(c => {
+                return (
+                    <div className="border">
+                        <h2 className="rant">{c.rant ? 'Rant!' : 'Rave!'}</h2>
+                        <h4>{c.content}</h4>
+                        <h3>
+                            <stong>- {c.author}</stong>
+                        </h3>
+                        <h4>Rating: {c.stars}</h4>
+                        <form method="POST" action={`/places/${data.place.id}/comment/${c.id}?_method=DELETE`}>
+                            <input type="submit" className="btn btn-danger" value="Delete Comment" />
+                        </form>
+                    </div>
+                )
+            })
+        }
     }
     return (
         <Def>
