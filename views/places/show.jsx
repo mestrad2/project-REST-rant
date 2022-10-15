d7ca1a212d1a1b9de60b56b53e6c85790fff3b97
Manuel Estrada <manuel.estrada16@gmail.com>
2022-10-15 02:10:40 -0400

    fixed edit and delete buttons

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -71,10 +71,10 @@ function show(data) {
                         <h4>
                             Serving {data.place.cuisines}
                         </h4>
-                        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+                        <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">
                             Edit
                         </a>
-                        <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+                        <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}>
                             <button type="submit" className="btn btn-danger">
                                 Delete
                             </button>
